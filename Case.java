af50c5571ebc9bbd4d37a90326532a4b45ec8c8b
Badarou <mikdaam.badarou@etu.upec.fr>
2020-03-19 13:04:24 +0000

    Projet Tuteuré

---
+++ Case.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Case {
+    public int num;
+    public boolean fixe;
+    public Case() {
+        this.num = 0;
+        this.fixe = false;
+    }
+    public Case(int n) {
+        this.num = n;
+        this.fixe = false;
+    }
+    public Case(int n, boolean f) {
+        this.num = 0;
+        this.fixe = f;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+    public int getNum() {
+        return num;
+    }
+
+    public void setFixe(boolean fixe) {
+        this.fixe = fixe;
+    }
+    public boolean isFixe() {
+        return fixe;
+    }
+}
---
+++ Jeu.java
@@ -0,0 +1,172 @@
+package com.company;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Jeu {
+    public Region[][] jeu;
+    public Jeu() {
+        jeu = new Region[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                jeu[i][j] = new Region();
+            }
+        }
+    }
+
+    public Jeu(File fichier) {
+        jeu = new Region[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                jeu[i][j] = new Region();
+            }
+        }
+        try {
+            Scanner sc = new Scanner(fichier);
+            String s;
+            int courant;
+        }catch (FileNotFoundException e)
+        {
+
+        }
+    }
+
+    public Region getRegion(int i, int j) {
+        return jeu[i][j];
+    }
+
+    public Region getRegionDeCase(int i, int j){
+        return (jeu[(int)(i/3)][(int)(j/3)]);
+    }
+
+    public void setRegion(Region[][] r){
+        jeu = r;
+    }
+
+    public int getCaseNum(int i, int j) {
+        return (jeu[(int)(i/3)][(int)(j/3)]).getCaseNum(i%3, j%3);
+    }
+
+    public void setCaseNum(int i, int j, int c) {
+        (jeu[(int)(i/3)][(int)(j/3)]).setCaseNum(i%3, j%3, c);
+    }
+
+    public boolean getCaseFixe(int i, int j) {
+        return (jeu[(int)(i/3)][(int)(j/3)]).getCaseFixe(i%3, j%3);
+    }
+
+    public boolean ligneCompte(int i) {
+        boolean un = false;
+        boolean deux = false;
+        boolean trois = false;
+        boolean quatre = false;
+        boolean cinq = false;
+        boolean six = false;
+        boolean sept = false;
+        boolean huit = false;
+        boolean neuf = false;
+
+        for (int j = 0; j < 9; j++) {
+            switch (this.getCaseNum(i, j)) {
+                case 1 : un = true; break;
+                case 2 : deux = true; break;
+                case 3 : trois = true; break;
+                case 4 : quatre = true; break;
+                case 5 : cinq = true; break;
+                case 6 : six = true; break;
+                case 7 : sept = true; break;
+                case 8 : huit = true; break;
+                case 9 : neuf = true; break;
+            }
+        }
+        if (un && deux && trois && quatre && cinq && six && sept && huit && neuf) {
+            return true;
+        }else
+            return false;
+    }
+
+    public boolean ligneBon(int i, int j, int val) {
+        if (val == 0)
+            return false;
+        for (int k = 0; k < 9; k++) {
+            if (val == this.getCaseNum(i, k))
+                return false;
+        }
+        return true;
+    }
+
+    public boolean coloneCompte(int j) {
+        boolean un = false;
+        boolean deux = false;
+        boolean trois = false;
+        boolean quatre = false;
+        boolean cinq = false;
+        boolean six = false;
+        boolean sept = false;
+        boolean huit = false;
+        boolean neuf = false;
+
+        for (int i = 0; i < 9; i++) {
+            switch (this.getCaseNum(i, j)) {
+                case 1 : un = true; break;
+                case 2 : deux = true; break;
+                case 3 : trois = true; break;
+                case 4 : quatre = true; break;
+                case 5 : cinq = true; break;
+                case 6 : six = true; break;
+                case 7 : sept = true; break;
+                case 8 : huit = true; break;
+                case 9 : neuf = true; break;
+            }
+        }
+        if (un && deux && trois && quatre && cinq && six && sept && huit && neuf) {
+            return true;
+        }else
+            return false;
+    }
+
+    public boolean coloneBon (int i, int j, int val) {
+        if (val == 0)
+            return false;
+        for (int k = 0; k < 9; k++) {
+            if (val == this.getCaseNum(k, j))
+                return false;
+        }
+        return true;
+    }
+
+    public boolean gagne() {
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++){
+                if (!(jeu[i][j]).regionCompte())
+                    return false;
+            }
+        }
+
+        for (int i = 0; i < 9; i++) {
+            if (!coloneCompte(i) || !ligneCompte(i))
+                return false;
+        }
+        return true;
+    }
+
+    public boolean resoudre(int i, int j) {
+        if (j == 9){
+            j = 0;
+            if (i++ == 9)
+                return true;
+        }
+        if (this.getCaseFixe(i, j))
+            return resoudre(i, j+1);
+        for (int val = 1; val <= 9; val++) {
+            if ((this.getRegionDeCase(i, j).regionBon(i, j, val)) && this.ligneBon(i, j, val) && this.coloneBon(i, j, val)) {
+                this.setCaseNum(i, j, val);
+                if (resoudre(i, j+1))
+                    return true;
+            }
+        }
+        setCaseNum(i, j, 0);
+        return false;
+    }
+}
